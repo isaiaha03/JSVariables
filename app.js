38a5a687f3d635ba5df79fa70684260fc877bf70
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-09-22 22:28:55 -0500

    Completed exercise.

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Yes";
+let lastName = "Man";
+var age = 22;
 // Exercise 2
-
+lastName = "No";
+age = 23;
 // Exercise 3
+let language = "JavaScript";
+let CreatedYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
